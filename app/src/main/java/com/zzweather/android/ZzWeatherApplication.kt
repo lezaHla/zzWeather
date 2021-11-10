9e0e23310fd7a740655512a9b409c36d5d07fb9b
lezaHla <1608536216@qq.com>
2021-11-10 14:04:22 +0800

    实现搜索全球城市数据功能

---
+++ app/src/main/java/com/zzweather/android/ZzWeatherApplication.kt
@@ -0,0 +1,18 @@
+package com.zzweather.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class ZzWeatherApplication: Application() {
+    companion object{
+        const val TOKEN = ""//在彩云天气申请到的令牌值，暂时未通过申请
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
